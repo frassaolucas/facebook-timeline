3df135bdcc5799b8756f742f1d54920eb12cc386
frassaolucas <frassao.lucas@gmail.com>
2020-04-27 07:57:07 -0300

    - 1st commit;

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./app.css";
+
+import Header from "./components/Header";
+import PostList from "./components/PostList";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <PostList />
+    </>
+  );
+}
+
+export default App;
